f416ef60baa054e3bcc9af5a7010bf3bc1c73e5f
duong-s-97 <duongdev97@gmail.com>
2024-04-25 13:09:47 +0700

    add: express-handlebars and set the views folder

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const { engine } = require("express-handlebars");
+const path = require("path");
+const express = require("express"); // import express
+const morgan = require("morgan"); // import morgan
+const app = express(); // chayj func express
+const port = 3000; // chạy prj trên cổng 3000
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// Set the views directory
+app.set("views", path.join(__dirname, "views"));
+
+//Template engine
+
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+
+//  định nghĩa route
+app.get("/", (req, res) => res.render("home"));
+app.get("/news", (req, res) => res.render("news"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
